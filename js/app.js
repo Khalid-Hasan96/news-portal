cc2d3129457b528734a268848f1d9ed1ddfad342
Khalid-Hasan96 <khbappy66@gmail.com>
2022-09-03 14:23:44 +0600

    card updated

--- js/app.js
+++ js/app.js
@@ -41,21 +41,32 @@ const displayNews = allNews => {
                 <div class="col-md-4">
                     <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-8 d-flex flex-column">
                     <div class="card-body">
-                        <h3 class="card-title">${news.title}</h3>
-                        <p class="card-text">${news.details.slice(0, 200)}....</p>
-                        <div class="d-flex justify-content-evenly align-items-center">
-                            <img src="${news.author.img}" width="40px" height="40px" class="rounded-circle">
-                            <p><small>${news.author.name ? news.author.name : 'No data found'}</small></p>
-                        </div>
                         <div>
-                            <p><i class="fa-solid fa-eye"></i>${news.total_view ? news.total_view : 'No data'}</p>
+                            <h3 class="card-title">${news.title}</h3>
+                            <p class="card-text text-muted">${news.details.slice(0, 200)}....</p>
                         </div>
-                    </div >
-                </div >
-            </div >
-        </div >
+                    </div> 
+                    <div class="card-footer text-muted d-flex justify-content-between">    
+                        
+                            <div class="d-flex">
+                                <img src="${news.author.img}" width="50px" height="50px" class="rounded-circle">
+                                <div>
+                                    <p class="fs-6 ps-2">${news.author.name ? news.author.name : 'Author not found'} <br> <small>${news.author.published_date ? news.author.published_date : 'No date found'}</small></p>
+                                    
+                                </div>
+                            </div>
+                            <div>
+                                <p class="fs-5"><i class="fa-solid fa-eye"></i>${news.total_view ? news.total_view : 'No data'}</p>
+                            </div>
+                            <p onclick="displayNewsDetail()" class="text-primary fs-4"><i class="fa-solid fa-arrow-right"></i></p>
+                                             
+                      
+                    </div>
+                </div>
+            </div>
+        </div>
     `;
         newsContainer.appendChild(newsDiv);
     });
