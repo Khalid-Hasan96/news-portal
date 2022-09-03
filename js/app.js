992948eb57cf06db399a9a5adc12444948dcd76a
Khalid-Hasan96 <khbappy66@gmail.com>
2022-09-04 01:20:22 +0600

    button working

--- js/app.js
+++ js/app.js
@@ -9,19 +9,20 @@ const displayCategories = categories => {
         `
     <div class="d-flex justify-content-between">
         <button type="button" class="btn btn-outline-primary">Home</button>
-        <button onclick="loadNews('${categories.category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[0].category_name}</button>
-        <button onclick="loadNews()" type="button" class="btn btn-outline-primary">${categories.news_category[1].category_name}</button>
-        <button onclick="loadNews()" type="button" class="btn btn-outline-primary">${categories.news_category[2].category_name}</button>
-        <button onclick="loadNews()" type="button" class="btn btn-outline-primary">${categories.news_category[3].category_name}</button>
-        <button onclick="loadNews()" type="button" class="btn btn-outline-primary">${categories.news_category[4].category_name}</button>
-        <button onclick="loadNews()" type="button" class="btn btn-outline-primary">${categories.news_category[5].category_name}</button>
-        <button onclick="loadNews()" type="button" class="btn btn-outline-primary">${categories.news_category[6].category_name}</button>
-        <button onclick="loadNews()" type="button" class="btn btn-outline-primary">${categories.news_category[7].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[0].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[0].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[1].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[1].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[2].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[2].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[3].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[3].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[4].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[4].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[5].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[5].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[6].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[6].category_name}</button>
+        <button onclick="loadNews('${categories.news_category[7].category_id}')" type="button" class="btn btn-outline-primary">${categories.news_category[7].category_name}</button>
     </div>
     `;
 }
-const loadNews = categoryId => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${categoryId}`;
+const loadNews = category_id => {
+    loadingSpinner(true);
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
     fetch(url)
         .then(res => res.json())
         .then(data => displayNews(data.data))
@@ -29,50 +30,54 @@ const loadNews = categoryId => {
 
 const displayNews = allNews => {
     const newsContainer = document.getElementById('news-container');
-    console.log(allNews)
-
+    newsContainer.innerHTML = ``;
     allNews.forEach(news => {
-        // console.log(news);
-        const newsDiv = document.createElement('div');
-        newsDiv.classList.add('col');
-        newsDiv.innerHTML = `
-        <div class="card mb-3">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
-                    <h1>${news.category_id}</h1>
-                </div>
-                <div class="col-md-8 d-flex flex-column">
-                    <div class="card-body">
-                        <div>
-                            <h4 class="card-title">${news.title}</h4>
-                            <p class="card-text text-muted">${news.details.slice(0, 200)}....</p>
-                        </div>
-                    </div> 
-                    <div class="card-footer text-muted d-flex justify-content-between">    
-
-                            <div class="d-flex">
-                                <img src="${news.author.img ? news.author.img : 'No Author Image'}" width="50px" height="50px" class="rounded-circle">
-                                <div>
-                                    <p class="fs-6 ps-2">${news.author.name ? news.author.name : 'No Author Found'} <br> <small>${news.author.published_date ? news.author.published_date : 'No date found'}</small></p>
-
-                                </div>
-                            </div>
-                            <div>
-                                <p class="fs-5"><i class="fa-solid fa-eye"></i> ${news.total_view ? news.total_view : 'No data'}</p>
-                            </div>
-
-                            <button onclick="loadNewsDetails('${news._id}')" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#newsModal">Read Full News</button>
-
-
+        console.log(news);
+        const divContainer = document.createElement('div');
+        divContainer.innerHTML = `
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8 d-flex flex-column">
+                <div class="card-body">
+                    <div>
+                        <h4 class="card-title">${news.title}</h4>
+                        <p class="card-text text-muted">${news.details.slice(0, 200)}....</p>
                     </div>
+                </div>
+                <div class="card-footer text-muted d-flex justify-content-between">
+
+                    <div class="d-flex">
+                        <img src="${news.author.img ? news.author.img : 'No Author Image'}" width="50px"
+                            height="50px" class="rounded-circle">
+                        <div>
+                            <p class="fs-6 ps-2">${news.author.name ? news.author.name : 'No Author Found'}
+                                <br> <small>${news.author.published_date ? news.author.published_date : 'No date found'}</small></p>
+
+                        </div>
+                    </div>
+                    <div>
+                        <p class="fs-5"><i class="fa-solid fa-eye"></i> ${news.total_view ? news.total_view : 'No data'}</p>
+                    </div>
+
+                    <button onclick="loadNewsDetails('${news._id}')" class="btn btn-outline-primary"
+                        data-bs-toggle="modal" data-bs-target="#newsModal">Read Full News</button>
                 </div>
             </div>
         </div>
-    `;
-        newsContainer.appendChild(newsDiv);
-    });
+    </div>
+
+        `;
+        newsContainer.appendChild(divContainer);
+    })
+    loadingSpinner(false);
 }
+
+
+
+
 
 const loadNewsDetails = newsId => {
     const url = `https://openapi.programming-hero.com/api/news/${newsId}`;
@@ -105,12 +110,27 @@ const displayNewsDetail = newsDetails => {
             <div class="card-footer text-muted text-center">
                 <img src="${news.author.img ? news.author.img : 'No Image Found'}"  width="50px" height="50px" class="rounded-circle">
                 <p>Author : ${news.author.name ? news.author.name : 'No Author Found'}</p>
-               
+
             </div>
         </div>
         `;
         modalBody.appendChild(bodyContainer);
     })
 }
-// loadNews('02');
+
+// loading spinner
+const loadingSpinner = isLoading => {
+    const spinnerSection = document.getElementById('loading');
+    if (isLoading) {
+        spinnerSection.classList.remove('d-none')
+    } else {
+        spinnerSection.classList.add('d-none')
+    }
+
+}
+
+const showItems = () => {
+    const itemsContainer = document.getElementById('items');
+    itemsContainer.innerText =
+}
 loadCategories();
